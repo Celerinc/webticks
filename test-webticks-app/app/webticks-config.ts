84fdb976eeb2fa4f5fe65d19473a0e7c8942704d
Abderrahman DAKIR ALLAH <ar.dakirallah@gmail.com>
2026-01-09 13:28:18 +0100

    update

--- test-webticks-app/app/webticks-config.ts
+++ test-webticks-app/app/webticks-config.ts
@@ -23,7 +23,7 @@ export default function WebticksConfig() {
         }
 
         const config = {
-            backendUrl: "http://localhost:3002/track",
+            backendUrl: "http://localhost:3002/api/track",
             appId: "97069816-8b25-4640-833f-f17259208a42"
         };
 
